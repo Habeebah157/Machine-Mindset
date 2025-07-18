d49efb9bbe284ea1594e93a129f26376a1904106
Habeebah Muse <omowumimuse@gmail.com>
2025-07-18 00:06:49 -0400

    first commit

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,8 +1,8 @@
 export const CONFIG = {
-	BLOG: {
-		username: "barbarbar338",
-		repo: "webpage",
-		repo_id: "MDEwOlJlcG9zaXRvcnkyNjc3OTQ5NDE=",
-	},
-	GITHUB_TOKEN: import.meta.env.VITE_GITHUB_TOKEN,
+  BLOG: {
+    username: "Habeebah157",
+    repo: "Machine-Mindset",
+    repo_id: "R_kgDOPOg91Q", // ← your actual repo ID
+  },
+  GITHUB_TOKEN: import.meta.env.VITE_GITHUB_TOKEN,
 };
